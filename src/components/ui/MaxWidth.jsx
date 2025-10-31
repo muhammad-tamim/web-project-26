8b03db1353b53a1eb71e46a64383c745d0632346
muhammad-tamim <contact2tamim@gmail.com>
2025-10-31 23:01:39 +0600

    refactor(MaxWidth.jsx): add max-w-1920 also with w-10/12

--- src/components/ui/MaxWidth.jsx
+++ src/components/ui/MaxWidth.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const MaxWidth = ({ children }) => {
     return (
-        <div className='w-11/12 mx-auto'>
+        <div className='w-10/12 mx-auto max-w-[1920px]'>
             {children}
         </div>
     );
