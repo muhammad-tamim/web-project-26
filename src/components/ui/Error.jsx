4655f0492bc0b61ba5a92eaa162ce9c74ff3d24d
muhammad-tamim <contact2tamim@gmail.com>
2025-10-31 20:17:06 +0600

    feat: add page not found page

--- src/components/ui/Error.jsx
+++ src/components/ui/Error.jsx
@@ -1,9 +1,27 @@
 import React from 'react';
+import { Link, Navigate, useRouteError } from 'react-router';
 
 const Error = () => {
+
+    const error = useRouteError()
+
+    if (error.status === 404) {
+        <Navigate to={'*'} replace={true} />
+    }
+
+    console.log(error)
     return (
-        <div>
-            Error
+        <div className="flex flex-col items-center justify-center min-h-screen text-center">
+            <h1 className="text-3xl font-bold mb-2">Something went wrong 😢</h1>
+            <p className="mb-4 text-gray-600">{error?.status || "Unknown"}</p>
+            <p className="mb-4 text-gray-600">{error?.statusText || "No status text"}</p>
+            <p className="mb-4 text-gray-600">{error?.message || "No message provided"}</p>
+            {error?.data && (
+                <p className="mb-4 text-gray-600">{JSON.stringify(error.data)}</p>
+            )}
+            <Link to="/" className="text-blue-600 underline">
+                Go Home
+            </Link>
         </div>
     );
 };
--- src/pages/PageNotFoundPage.jsx
+++ src/pages/PageNotFoundPage.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
+import { Link, useLocation } from 'react-router';
 
 const PageNotFoundPage = () => {
+    const location = useLocation();
     return (
         <div>
-            Page not found
+            <div className='min-h-[calc(100vh-105px)] flex flex-col items-center justify-center space-y-4'>
+                <h1 className='font-bold text-5xl'>404</h1>
+                <p className='text-primary-content/70 text-2xl'>No route matches URL "{location.pathname}"</p>
+                <Link to={'/'}><button className='btn btn-primary rounded-full text-white btn-xl'>Home Page</button></Link>
+            </div>
         </div>
     );
 };
