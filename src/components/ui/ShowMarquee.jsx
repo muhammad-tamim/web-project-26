17e40a6dfe94df04e662c2ab531dc04e98667588
muhammad-tamim <contact2tamim@gmail.com>
2025-10-31 23:02:52 +0600

    refactor(ShowMarquee): add real links to the title

--- src/components/ui/ShowMarquee.jsx
+++ src/components/ui/ShowMarquee.jsx
@@ -1,5 +1,6 @@
 import React, { use } from 'react';
 import Marquee from "react-fast-marquee";
+import { Link } from 'react-router';
 
 const ShowMarquee = ({ fetchData }) => {
     const data = use(fetchData)
@@ -7,7 +8,7 @@ const ShowMarquee = ({ fetchData }) => {
     const latestNews = data.filter((news) => news.others.is_today_pick === true)
     return (
         <Marquee pauseOnHover={true}>
-            {latestNews.map((news) => <span className='mr-5' key={news.id}>{news.title}</span>)}
+            {latestNews.map((news) => <Link to={`/news-details/${news.id}`} className='mr-5 cursor-pointer hover:underline' key={news.id}>{news.title}</Link>)}
         </Marquee>
     );
 };
