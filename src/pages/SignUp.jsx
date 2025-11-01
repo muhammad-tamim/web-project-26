98eb67850887424c0d9f7c40b4f891ad1e12860a
muhammad-tamim <contact2tamim@gmail.com>
2025-11-01 22:33:04 +0600

    feat(SignUp): implement full signUp functionality

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,13 +1,48 @@
-import React from 'react';
-import { Link } from 'react-router';
+import React, { use } from 'react';
+import { Link, replace, useNavigate } from 'react-router';
+import { AuthContext } from '../contexts/AuthContext';
+import toast from 'react-hot-toast';
 
 const SignUp = () => {
+
+    const { signUpUser, setUser, updateUserInfo } = use(AuthContext)
+
+    const navigate = useNavigate()
+
+    const handleSignUp = (e) => {
+        e.preventDefault()
+
+        const name = e.target.name.value
+        const url = e.target.url.value
+        const email = e.target.email.value
+        const password = e.target.password.value
+
+        signUpUser(email, password)
+            .then((result) => {
+                toast.success('Login Successful')
+                const user = result.user
+
+                updateUserInfo({ displayName: name, photoURL: url })
+                    .then(() => {
+                        setUser({ ...user, displayName: name, photoURL: url })
+                        navigate('/news/1')
+                    })
+                    .catch(() => {
+                        setUser(user)
+                    })
+
+            })
+            .catch((error) => {
+                console.log(error)
+            })
+    }
+
     return (
         <div className='my-10 max-w-[650px] mx-auto w-full px-[73px] py-[76px] border border-gray-50 rounded-md shadow space-y-8'>
             <h1 className='text-center font-semibold text-4xl dark2'>Register your account</h1>
             <hr className='text-[#E7E7E7]' />
             <div className='px-4'>
-                <form className='flex flex-col'>
+                <form onSubmit={handleSignUp} className='flex flex-col'>
                     <label className="font-semibold text-xl dark2 mb-4">Your Name</label>
                     <input type="text" name='name' className="input input-xl text-base w-full dark7 outline-0 mb-6" placeholder="Enter your name" />
                     <label className="font-semibold text-xl dark2 mb-4">Photo URL</label>
