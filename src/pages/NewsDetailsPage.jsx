a1bdc84a1a5c7968e04545182111848faca1f560
muhammad-tamim <contact2tamim@gmail.com>
2025-11-01 17:06:27 +0600

    style(NewsDetailsPage.jsx): make it responsive

--- src/pages/NewsDetailsPage.jsx
+++ src/pages/NewsDetailsPage.jsx
@@ -18,10 +18,10 @@ const NewsDetailsPage = () => {
     const { title, thumbnail_url, details, category_id } = singleData
 
     return (
-        <div className='border border-[#E7E7E7] rounded-md p-6 space-y-5'>
+        <div className='border border-[#E7E7E7] rounded-md p-3 md:p-6 space-y-5'>
             <img className='rounded-md w-full' src={thumbnail_url} alt="" />
-            <h1 className='font-bold text-2xl dark2 max-w-[791px]'>{title}</h1>
-            <p className='dark3'>{details}</p>
+            <h1 className='font-bold md:text-2xl dark2 md:max-w-[791px]'>{title}</h1>
+            <p className='dark3 text-sm md:text-base'>{details}</p>
             <Link to={`/news/${category_id}`} className='flex items-center justify-start w-fit gap-1 btn btn-primary text-white font-medium'><GoArrowLeft size={20} /> <span>All news in this category</span></Link>
         </div>
     );
