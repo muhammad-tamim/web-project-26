034cf8bb4b3faa5f7dcf67a70cbec8f2ff13f674
muhammad-tamim <contact2tamim@gmail.com>
2025-11-01 23:11:08 +0600

    feat: add google and github login

--- src/components/structure/rightSide/SocialLogin.jsx
+++ src/components/structure/rightSide/SocialLogin.jsx
@@ -1,19 +1,43 @@
-import React from 'react';
+import React, { use } from 'react';
 import { FaGithub, FaGoogle } from 'react-icons/fa';
+import { AuthContext } from '../../../contexts/AuthContext';
+import toast from 'react-hot-toast';
 
 const SocialLogin = () => {
+    const { signInUserWithGoogle, signInUserWithGithub } = use(AuthContext);
+
+    const handleGoogleSignIn = () => {
+        signInUserWithGoogle()
+            .then(() => {
+                toast.success("SignIn successful");
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+    }
+
+    const handleGithubSignIn = () => {
+        signInUserWithGithub()
+            .then(() => {
+                toast.success("SignIn successful");
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+    }
+
     return (
         <div>
             <p className='font-semibold text-xl dark2 mb-5'>Login With</p>
             <div className='space-y-4'>
                 {/* Google */}
-                <button className="w-full btn btn-outline text-blue-500">
+                <button onClick={handleGoogleSignIn} className="w-full btn btn-outline text-blue-500">
                     <FaGoogle></FaGoogle>
                     Login with Google
                 </button>
 
                 {/* GitHub */}
-                <button className="w-full btn btn-outline dark2">
+                <button onClick={handleGithubSignIn} className="w-full btn btn-outline dark2">
                     <FaGithub></FaGithub>
                     Login with Github
                 </button>
