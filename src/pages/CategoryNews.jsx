84fa33e6555f467f4ac43a246c99f879eb7afe0f
muhammad-tamim <contact2tamim@gmail.com>
2025-11-01 14:25:50 +0600

    feat: add category news

--- src/pages/CategoryNews.jsx
+++ src/pages/CategoryNews.jsx
@@ -1,11 +1,31 @@
-import React from 'react';
-import { useParams } from 'react-router';
+import React, { useEffect, useState } from 'react';
+import { useLoaderData, useParams } from 'react-router';
+import CategoryNewsCard from '../components/ui/CategoryNewsCard';
 
 const CategoryNews = () => {
+    const [data, setData] = useState([])
     const { id } = useParams()
+    const intId = parseInt(id)
+    const news = useLoaderData()
+
+    useEffect(() => {
+        if (intId === 0) {
+            setData(news)
+        }
+        else if (intId === 1) {
+            const filteredId = news.filter((singleNews) => singleNews.others.is_today_pick === true)
+            setData(filteredId)
+        }
+        else {
+            const filteredId = news.filter((singleNews) => singleNews.category_id === intId)
+            setData(filteredId)
+        }
+    }, [news, intId])
+
     return (
         <div>
-            <h1>{id}</h1>
+            <p className='font-semibold text-xl dark2 mb-5'>Dragon News Home</p>
+            {data.map((singleData) => <CategoryNewsCard key={singleData.id} singleData={singleData}></CategoryNewsCard>)}
         </div>
     );
 };
