7387da2c40295495e83f61282cee98c501dd17e5
muhammad-tamim <contact2tamim@gmail.com>
2025-11-01 18:44:59 +0600

    feat: add auth layout

--- src/layouts/AuthLayout.jsx
+++ src/layouts/AuthLayout.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
+import Navbar from '../components/structure/Navbar';
+import { Outlet } from 'react-router';
 
 const AuthLayout = () => {
     return (
-        <div>
-            authLayout
+        <div >
+            <div >
+                <Navbar></Navbar>
+            </div>
+            <div className='min-h-[calc(100vh-76px)] flex flex-col justify-center items-center'>
+                <Outlet></Outlet>
+            </div>
         </div>
     );
 };
