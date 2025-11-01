ac03d7bfb74b9bec317c3a43170884b5d74664c6
muhammad-tamim <contact2tamim@gmail.com>
2025-11-01 22:33:14 +0600

    feat(SignIn): implement full signIn functionality

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,13 +1,37 @@
-import React from 'react';
-import { Link } from 'react-router';
+import React, { use } from 'react';
+import { Link, useLocation, useNavigate } from 'react-router';
+import { AuthContext } from '../contexts/AuthContext';
+import toast from 'react-hot-toast';
 
 const SignIn = () => {
+
+    const { signInUser } = use(AuthContext)
+    const location = useLocation()
+    const navigate = useNavigate()
+
+    const handleSignIn = (e) => {
+        e.preventDefault()
+
+        const email = e.target.email.value
+        const password = e.target.password.value
+
+        signInUser(email, password)
+            .then(() => {
+                toast.success('Login Successful')
+                navigate(location?.state || '/news/1')
+            })
+            .catch((error) => {
+                console.log(error.message)
+            })
+    }
+
+
     return (
         <div className='max-w-[650px] mx-auto w-full px-[73px] py-[76px] border border-gray-50 rounded-md shadow space-y-10'>
             <h1 className='text-center font-semibold text-4xl dark2'>Login Your Account</h1>
             <hr className='text-[#E7E7E7]' />
             <div className='px-4'>
-                <form className='flex flex-col'>
+                <form onSubmit={handleSignIn} className='flex flex-col'>
                     <label className="font-semibold text-xl dark2 mb-4">Email address</label>
                     <input type="email" name='email' className="input input-xl text-base w-full dark7 outline-0 mb-6" placeholder="Enter your email address" />
                     <label className="font-semibold text-xl dark2 mb-4">Password</label>
